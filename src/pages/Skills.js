b0def6d8df2454481cf314825dea0a1bab5f81f9
praduman <praduman@pradumans-MacBook-Pro.local>
2024-03-22 04:12:30 +0530

    ui changes

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import js from '../assets/images/js.png'
 import ts from '../assets/images/ts.png'
+import java from '../assets/images/java.png'
 import html from '../assets/images/html.png'
 import css from '../assets/images/css.png'
 import redux from '../assets/images/redux.png'
+import rest from '../assets/images/rest.png'
+import websocket from '../assets/images/websocket.png'
+import oauth from '../assets/images/oauth.png'
+import jwt from '../assets/images/jwt.png'
+import nodejs from '../assets/images/nodejs.png'
+import express from '../assets/images/express.png'
 import SkillsImage from '../components/SkillsImage'
 import react from '../assets/images/reactnative.png'
 import vs from '../assets/images/vs.png'
@@ -11,10 +18,27 @@ import androidstudio from '../assets/images/androidstudio.png'
 import git from '../assets/images/git.png'
 import github from '../assets/images/github.png'
 import gitlab from '../assets/images/gitlab.png'
-
+import next from '../assets/images/nextjs.webp'
+import ssl from '../assets/images/ssl.jpeg'
+import nginx from '../assets/images/nginx.png'
+import sonar from '../assets/images/sonar.png'
+import jest from '../assets/images/jest.png'
+import azure from '../assets/images/azure.png'
+import aws from '../assets/images/aws.png'
+import docker from '../assets/images/docker.png'
+import cicd from '../assets/images/cicd.png'
+import mongo from '../assets/images/mongodb.png'
+import mui from '../assets/images/mui.png'
+import styled from '../assets/images/styled.jpeg'
+import scss from '../assets/images/scss.jpeg'
+import app from '../assets/images/app.png'
+import play from '../assets/images/play.png'
+import webpack from '../assets/images/webpack.png'
+import xcode from '../assets/images/xcode.jpeg'
+import babel from '../assets/images/babel.png'
 const Skills = () => {
   return (
-      <div id="skills" className='container-fluid' style={{ minHeight: '100vh' }}>
+      <div id="skills" className='container-fluid' style={{ minHeight: '100vh' ,background:"#f9f9f9"}}>
       <h1 className='row justify-content-center p-5 text-decoration-underline'>Skills</h1>
         <div
           className="row g-4"
@@ -24,6 +48,7 @@ const Skills = () => {
             <div className='card d-flex flex-column shadow '>
               <SkillsImage image={js} title={"JavaScript"} />
               <SkillsImage image={ts} title={"TypeScript"} />
+              <SkillsImage image={java} title={"Java"} />
             </div>
             <h4 style={{fontWeight:'bold'}}className='mt-4'> IDE & Version Controler</h4>
             <div className='card d-flex flex-column shadow '>
@@ -38,58 +63,66 @@ const Skills = () => {
           <h4 style={{fontWeight:'bold'}}> Frontend</h4>
             <div className='card d-flex flex-column shadow'>
               <SkillsImage image={html} title={"Html"} />
-              <SkillsImage image={css} title={"Css"} />
               <SkillsImage image={react} title={"Reactjs"} />
               <SkillsImage image={react} title={"React Native"} />
               <SkillsImage image={redux} title={"Redux"} />
-              <SkillsImage image={redux} title={"Nextjs"} />
-              <SkillsImage image={redux} title={"REST"} />
-              <SkillsImage image={redux} title={"Websockets"} />
-              <SkillsImage image={redux} title={"OAuth"} />
-              <SkillsImage image={redux} title={"JWT"} />
+              <SkillsImage image={next} title={"Nextjs"} />
+              <SkillsImage image={rest} title={"REST"} />
+              <SkillsImage image={websocket} title={"Websockets"} />
+              <SkillsImage image={oauth} title={"OAuth"} />
+              <SkillsImage image={jwt} title={"JWT"} />
             </div>
           </div>
           <div className="col-md-2">
           <h4 style={{fontWeight:'bold'}}> Backend</h4>
             <div className='card d-flex flex-column shadow'>
-              <SkillsImage image={html} title={"Nodejs"} />
-              <SkillsImage image={css} title={"Expressjs"} />
-              <SkillsImage image={react} title={"Firebase"} />
-              <SkillsImage image={react} title={"React Native"} />
+              <SkillsImage image={nodejs} title={"Nodejs"} />
+              <SkillsImage image={express} title={"Expressjs"} />
+              <SkillsImage image={nginx} title={"Nginx"} />
+              <SkillsImage image={ssl} title={"SSl"}/>
             </div>
-            <h4 style={{fontWeight:'bold'}} className='mt-4'> Database</h4>
-            <div className=' d-flex flex-column '>
-              <SkillsImage image={html} title={"Mongodb"} />
+            <h4 style={{fontWeight:'bold'}} className='mt-4'> Build</h4>
+            <div className='card d-flex flex-column shadow'>
+              <SkillsImage image={webpack} title={"Webpack"} />
+              <SkillsImage image={babel} title={"Babel"} />
+              <SkillsImage image={xcode} title={"Xcode"} />
+              <SkillsImage image={androidstudio} title={"Android studio"} />
             </div>
+           
           </div>
+          
           <div className="col-md-2 ">
-           <h4 style={{fontWeight:'bold'}}> More Styling</h4>
+          <h4 style={{fontWeight:'bold'}} > Testing</h4>
             <div className='card d-flex flex-column shadow'>
-              <SkillsImage image={html} title={"Styled Components"} />
-              <SkillsImage image={html} title={"Scss"} />
-              <SkillsImage image={html} title={"Material ui"} />
-              <SkillsImage image={html} title={"Ant design"} />
+              <SkillsImage image={jest} title={"Jest"} />
+              <SkillsImage image={sonar} title={"SonarQube"} />
+             
 
-            </div>
-          </div>
-          <div className="col-md-3 ">
-          <h4 style={{fontWeight:'bold'}}> Build</h4>
-            <div className='card d-flex flex-column shadow'>
-              <SkillsImage image={html} title={"Webpack"} />
-              <SkillsImage image={html} title={"Babel"} />
-              <SkillsImage image={html} title={"Xcode"} />
-              <SkillsImage image={html} title={"Android studio"} />
             </div>
             <h4 style={{fontWeight:'bold'}}className='mt-4'>Deployment</h4>
             <div className='card d-flex flex-column shadow'>
-              <SkillsImage image={html} title={"Microsoft Azure"} />
-              <SkillsImage image={html} title={"Github pages"} />
-              <SkillsImage image={html} title={"Netlify"} />
-              <SkillsImage  title={"Firebase"} />
-
+              <SkillsImage image={azure} title={"Microsoft Azure"} />
+              <SkillsImage image={docker} title={"Docker"} />
+              <SkillsImage image={aws} title={"AWS"} />
+              <SkillsImage image={cicd} title={"CI/CD Pipeline"} />
+              <SkillsImage image={play} title={"Play store"} />
+              <SkillsImage image={app} title={"App Store"} />
             </div>
           </div>
+          <div className="col-md-2 ">
+           <h4 style={{fontWeight:'bold'}}>Styling</h4>
+            <div className='card d-flex flex-column shadow'>
+            <SkillsImage image={css} title={"Css"} />
+              <SkillsImage image={styled} title={"Styled Components"} />
+              <SkillsImage image={scss} title={"Scss"} />
+              <SkillsImage image={mui} title={"Material UI"} />
 
+            </div>
+            <h4 style={{fontWeight:'bold'}} className='mt-4'> Database</h4>
+            <div className='card  d-flex flex-column shadow'>
+              <SkillsImage image={mongo} title={"Mongodb"} />
+            </div>
+          </div>
         </div>
       </div>
 
